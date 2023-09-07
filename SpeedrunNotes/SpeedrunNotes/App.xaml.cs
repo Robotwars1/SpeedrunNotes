ec084089b1e0a82ac627942fcf0b1e206dd7d6a7
Robotwars1 <magnus.carlsson4@gmail.com>
2023-09-07 20:07:50 +0200

    Feat. Next split indicator

--- SpeedrunNotes/SpeedrunNotes/App.xaml.cs
+++ SpeedrunNotes/SpeedrunNotes/App.xaml.cs
@@ -9,6 +9,7 @@ public partial class App : Application
         // Gets the value of preferences
         // If preference == null, set default value
         // Value of the users set preference is the same before as after update
+        // Default values == Livesplit.server recomended defaults
         Preferences.Default.Get("IP", "localhost");
         Preferences.Default.Get("Port", 16834);
 
