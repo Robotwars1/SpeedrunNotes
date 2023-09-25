978197d96c472d5d7d40985257398d09d09dcc1d
Robotwars1 <magnus.carlsson4@gmail.com>
2023-09-25 20:30:46 +0200

    Feat. VS Project Creation

---
+++ SpeedrunNotesEditor/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SpeedrunNotesEditor;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
