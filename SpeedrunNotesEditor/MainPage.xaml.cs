10b11cb01b86e84af06709f48277f9a65985a360
Robotwars1 <magnus.carlsson4@gmail.com>
2023-09-28 21:58:52 +0200

    Feat. Get amount of splits from .lss files

--- SpeedrunNotesEditor/MainPage.xaml.cs
+++ SpeedrunNotesEditor/MainPage.xaml.cs
@@ -1,7 +1,11 @@
-﻿namespace SpeedrunNotesEditor;
+﻿using System.Xml;
+
+namespace SpeedrunNotesEditor;
 
 public partial class MainPage : ContentPage
 {
+	int SplitsAmount;
+
 	public MainPage()
 	{
 		InitializeComponent();
@@ -17,8 +21,28 @@ public partial class MainPage : ContentPage
 
 	}
 
-	void OnSelectSplitPresetClicked(object sender, EventArgs e)
+	async void OnSelectSplitPresetClicked(object sender, EventArgs e)
 	{
+		var SplitFile = await FilePicker.PickAsync(default);
 
+		string FilePath = SplitFile.FullPath;
+
+		lssParse(FilePath);
+	}
+
+	// Function for getting the relevant data out of the selected .lss file
+	void lssParse(string FilePath)
+	{
+		XmlTextReader Reader = new XmlTextReader(FilePath);
+
+		while (Reader.Read())
+		{
+			switch (Reader.NodeType)
+			{
+				case XmlNodeType.Element:
+					SplitsAmount++;
+					break;
+			}
+		}
 	}
 }
